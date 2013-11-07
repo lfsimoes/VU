af6a1941925ce11fd063d9e714e27ab51cefd523
Luís F. Simões <luis.simoes@vu.nl>
2013-11-07 14:58:10 +0100

    new section in 1.3, on shortest paths
    new examples also provided of how to use set operations to obtain
    different kinds of information from the co-authorship network

--- 2013__Collective_Intelligence/13__network_analysis.py
+++ 2013__Collective_Intelligence/13__network_analysis.py
@@ -8,7 +8,7 @@
 # <markdowncell>
 
 # [Luís F. Simões](mailto:luis.simoes@vu.nl)<br>
-# 2013-11-06<div style="float: right">`Notebooks:` [&larr; previous](http://nbviewer.ipython.org/urls/raw.github.com/lfsimoes/VU/master/2013__Collective_Intelligence/12__inspecting_the_data.ipynb) &bull; [index &uarr;](https://github.com/lfsimoes/VU/tree/master/2013__Collective_Intelligence)</div><br><br>
+# 2013-11-06 *(updated: 2013-11-07)*<div style="float: right">`Notebooks:` [&larr; previous](http://nbviewer.ipython.org/urls/raw.github.com/lfsimoes/VU/master/2013__Collective_Intelligence/12__inspecting_the_data.ipynb) &bull; [index &uarr;](https://github.com/lfsimoes/VU/tree/master/2013__Collective_Intelligence)</div><br><br>
 # 
 # *******
 
@@ -109,15 +109,16 @@ display_summary( 23144668 )
 
 # `Summaries` maps paper *ids* to paper *summaries*. Let us now create here mappings by different criteria.
 # 
-# We'll start by building a mapping from *authors*, to *ids* of papers they authored:
+# We'll start by building a mapping from *authors*, to the set of *ids* of papers they authored. 
+# We'll be using Python's [sets](http://docs.python.org/2/library/stdtypes.html#set-types-set-frozenset) for that purpose.
 
 # <codecell>
 
-papers_of_author = defaultdict(list)
+papers_of_author = defaultdict(set)
 
 for id,p in Summaries.iteritems():
     for a in p.authors:
-        papers_of_author[ a ].append( id )
+        papers_of_author[ a ].add( id )
 
 # <codecell>
 
@@ -131,8 +132,6 @@ for id in papers_of_author['Eiben AE']:
 # <markdowncell>
 
 # We now build a co-authorship network, a graph linking *authors*, to the set of *co-authors* they have published with.
-# 
-# To obtain the union of author lists, we'll be using Python's [sets](http://docs.python.org/2/library/stdtypes.html#set-types-set-frozenset).
 
 # <codecell>
 
@@ -149,6 +148,57 @@ for a,ca in coauthors.iteritems():
 # <codecell>
 
 print ', '.join( coauthors['Eiben AE'] )
+
+# <markdowncell>
+
+# Because both an author's papers, and its collaborators are stored as Python sets, we can then use [set operations](http://docs.python.org/2/library/stdtypes.html#set-types-set-frozenset) to easily combine different sets, with low computational complexity. Here are a few examples.
+# 
+# The intersection of two author's publication records, can be used to obtain the set of papers they co-authored together:
+
+# <codecell>
+
+for id in papers_of_author['Wilson EO'] & papers_of_author['Nowak MA']:
+    display_summary( id )
+
+# <markdowncell>
+
+# The intersection of two author's collaborators sets, identifies people they have both published with:
+
+# <codecell>
+
+coauthors[u'Bäck T'] & coauthors['Haasdijk E']
+
+# <markdowncell>
+
+# Set difference, allows us to remove from an author's publication record, those papers that were published with a given author:
+
+# <codecell>
+
+for id in papers_of_author['Eiben AE'] - papers_of_author['Haasdijk E']:
+    display_summary( id )
+
+# <markdowncell>
+
+# Imagine you know who the members of a research group are, and want to obtain the union of all papers published by the group's members. Most certainly, there is plenty of collaboration among group members, with many papers authored together, but we want to obtain a list of publications with no duplicate entries. Here is a way to achieve that, using Python's [reduce](http://docs.python.org/2/library/functions.html#reduce) function, to iteratively perform a union of all their publication records. We take as example three current or former members from the [IRIDIA](http://code.ulb.ac.be/iridia.home.php) lab. The make the list shorter, we show only 5 of the resulting papers.
+
+# <codecell>
+
+authors = [ 'Dorigo M', 'Trianni V', 'Lenaerts T' ]
+
+group_publications = reduce( set.union, [ papers_of_author[a] for a in authors ], set() )
+
+for id in list(group_publications)[5:10]:
+    display_summary( id )
+
+# <headingcell level=3>
+
+# Network statistics
+
+# <codecell>
+
+print 'Number of nodes: %8d (node == author)' % len(coauthors)
+print 'Number of links: %8d (link == collaboration between the two linked authors on at least one paper)'  \
+        % sum( len(cas) for cas in coauthors.itervalues() )
 
 # <headingcell level=4>
 
@@ -288,7 +338,7 @@ cited_by = dict(cited_by)
 
 # <headingcell level=3>
 
-# Dataset statistics
+# Network statistics
 
 # <markdowncell>
 
@@ -341,7 +391,7 @@ print 'Citations by ids from outside the dataset comprise %d (%.2f %%) of that t
 
 # <markdowncell>
 
-# Let us now find which are the 10 most cited papers in our dataset.
+# Let us now find which 10 papers are the most cited in our dataset.
 
 # <codecell>
 
@@ -401,4 +451,123 @@ plt.ylabel('fraction of papers')
 plt.yscale('log')
 #plt.xscale('log')
 plt.xlim(0,20);
+
+# <headingcell level=2>
+
+# The Six Degrees of Robert Axelrod
+
+# <markdowncell>
+
+# There's a famous kind of "game", involving collaboration networks, that consists in naming the links along the shortest path in the network that connects any given person to a reference one. Famous examples include:
+# 
+# * the [Six Degrees of Kevin Bacon](https://en.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon), which connects any actor through a chain of movies, eventually ending in Kevin Bacon (try entering different actors in the [Oracle of Bacon](http://oracleofbacon.org/));
+#   * *here's an example from Wikipedia:<br>Elvis Presley was in Change of Habit (1969) with Edward Asner; Edward Asner was in JFK (1991) with Kevin Bacon.*<br>Therefore, Asner has a Bacon number of 1, and Presley a Bacon number of 2.
+# * the [Erdős number](https://en.wikipedia.org/wiki/Erd%C5%91s_number), which measures a scientist's distance in the co-authorship network to the great Hungarian mathematician [Paul Erdős](https://en.wikipedia.org/wiki/Paul_Erd%C5%91s).
+# 
+# Interesting as those numbers may be, though, they pale in comparison to the one true number, the one that identifies the few truly exceptional individuals: the [Erdős-Bacon-Sabbath number](http://erdosbaconsabbath.com/)! If you have published a scientific paper, been involved in a movie or TV show, and released a song (and in all three networks there is a path taking you to Paul Erdős, Kevin Bacon, and the mighty Black Sabbath, respectively), then mankind bows to your greatness. Surprisingly, many people do have such a number (see for instance, [Natalie Portman](http://erdosbaconsabbath.com/natalie-portman/))! You can find [here](http://erdosbaconsabbath.com/category/ebs/) the full list of luminaries for whom an Erdős-Bacon-Sabbath number has been identified.
+# 
+# Unfortunately, neither Paul Erdős, nor Kevin Bacon nor Black Sabbath are in our dataset, so, if we want to play that game, we need to pick someone else to be our reference node.
+# 
+# Given we'll be walking through a network of scientific collaborations, it then seems naturally fitting that we choose [Robert Axelrod](https://en.wikipedia.org/wiki/Robert_Axelrod) as our reference node, given his highly influential work on the [evolution of cooperation](https://en.wikipedia.org/wiki/The_Evolution_of_Cooperation).
+
+# <codecell>
+
+print '%d publications by Robert Axelrod found in the dataset,'  \
+      ' authored with a total of %d collaborators,'  \
+      ' and cited a total of %d times.' % (
+    len( papers_of_author['Axelrod R'] ),
+    len( coauthors['Axelrod R'] ),
+    sum( len(papers_citing[id]) for id in papers_of_author['Axelrod R'] ) )
+
+display_summary( 7466396, ', nr. citations: %d' % len(papers_citing[7466396]) )
+
+# <headingcell level=3>
+
+# Shortest paths
+
+# <markdowncell>
+
+# Playing the degrees of separation game requires the capacity to traverse the graph in a way that enables efficient identification of the shortest paths between any two given nodes. See the Wikipedia page on the [shortest path problem](https://en.wikipedia.org/wiki/Shortest_path_problem) for different ways of implementing such a search.
+# 
+# The code below can be seen as a special case of [Dijkstra's algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm). Should you want for some reason to convert our networks into weighted ones (with links having weights indicating somehing like inverse frequency with which two authors collaborate, for instance), then you need to extend the code below into a full implementation of Dijkstra's algorithm.
+# 
+# This code uses [deque](http://docs.python.org/2/library/collections.html#collections.deque) objects, a generalization of stacks and queues, that allows for efficient insertions and removals at either end of the sequence.
+
+# <codecell>
+
+def shortest_path( graph, node_from, node_to ):
+    """
+    Performs a breadth-first search on the (unweighted) graph
+    for the shortest path between two nodes.
+    """
+    # dictionary indicating our graph traversal at some point visited `node`, coming from node `visited[node]`
+    visited = { node_from : None }
+    
+    # FIFO queue, used to perform a breadth-first expansion of the as-yet unbranched nodes
+    Q = deque( [ node_from ] )
+    
+    while len(Q) > 0:
+        
+        u = Q.popleft()
+        
+        # final node found. rebuild shortest path between source and destination nodes.
+        if u == node_to:
+            at = u
+            path = deque([ at ])
+            while True:
+                at = visited[ at ]
+                path.appendleft( at )
+                if at == node_from:
+                    return path
+        
+        # pick those nodes `u` links to, that we haven't encountered yet
+        n_neighbs = [ n for n in graph[u] if n not in visited ]
+        
+        Q.extend( n_neighbs )
+        for n in n_neighbs:
+            visited[ n ] = u
+    
+    return None  # no path found
+
+# <markdowncell>
+
+# In addition to knowking a path (sequence of nodes), we'll also want to know some information about the links that make such path possible.
+
+# <codecell>
+
+def show_papers_along_path( authors_path ):
+    """
+    Given a sequence of authors, such as the one produced by shortest_path(), prints out
+    one selected paper co-authored by each consecutive pair of authors in the sequence.
+    The most cited publication from each collaboration is selected for display.
+    """
+    a1 = None
+    for a2 in authors_path:
+        
+        if a1 is None:
+            a1 = a2
+            continue
+        
+        # find the most cited paper coauthored by `a1` and `a2`
+        collab = max( papers_of_author[a1] & papers_of_author[a2], key=lambda i:len(papers_citing[i]) )
+        
+        display_summary( collab, ', nr. citations: %d, collaboration between %s and %s' % ( len(papers_citing[collab]), a1, a2 ) )
+        
+        a1 = a2
+
+# <markdowncell>
+
+# Using the above code, we can now query for an author's Axelrod number, and list the sequence of links along the sortest path between them.
+
+# <codecell>
+
+author = 'Adami C'
+#author = 'Nowak MA'
+path = shortest_path( coauthors, author, 'Axelrod R' )
+print '%s has an Axelrod number of %d.' % (author, len(path)-1)
+path
+
+# <codecell>
+
+show_papers_along_path( path )
 
